ac67e29ebc1b0eddca9686d3a3921c52b241366d
Brad Hambrice <hambrice@uab.edu>
2018-01-07 07:17:05 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,14 +1,11 @@
 
 class School
 def initialize(name)
+  @roster = {}
   @name = name
 end
-def roster
-  roster = Hash.new
-end
 def add_student(student_name, grade)
-  roster
-    roster[grade] = []
-  roster[grade] << student_name
+    @roster[grade] = []
+  @roster[grade] << student_name
 end
 end
